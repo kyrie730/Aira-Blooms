f7b4cdcdb0eed5110349058f0ead3f84e646693e
Adrianto Wijaya <adrianto.wijaya@verihubs.com>
2023-09-22 18:23:43 +0700

    init repo

---
+++ src/shared/Constant/Assets.ts
@@ -0,0 +1,7 @@
+export enum Images {
+  DgiftLogo = "/assets/images/im_dgift_logo.png",
+}
+
+export enum Illustrations {
+  HomeBanner = "/assets/illustrations/il_home_banner.jpg",
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        preload: "preload-loading 1.5s infinite ease-in-out",
+      },
+      colors: {
+        background: "#000000",
+        black: "#000000",
+        primary: {
+          DEFAULT: "#c6a548",
+          light: "#d7c07e",
+          dark: "#004EA3",
+        },
+      },
+      fontFamily: {
+        lato: ["var(--font-lato)"],
+      },
+      keyframes: {
+        "preload-loading": {
+          "0%, 100%": {
+            transform: "translate(-52px, 0)",
+          },
+          "50%": {
+            transform: "translate(96px, 0)",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
